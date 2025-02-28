611a4396ddbcbe56af8205f8dbc35f3f31599558
Martin Barker <martinbarker99@gmail.com>
2025-02-28 10:30:17 -0800

    add install_requires[] to setup.py. add tiffInput flag for matching testing. add flag support in fli. add example command to readme

--- setup.py
+++ setup.py
@@ -4,10 +4,25 @@ setup(
     name="Rhapso",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        'numpy',
+        'boto3',
+        'pandas',
+        'matplotlib',
+        'dask[array]',  
+        'zarr',
+        's3fs',
+        'scipy',
+        'scikit-image',
+        'bioio',
+        'bioio-tifffile',
+        'tifffile==2025.1.10',
+        'opencv-python',  
+    ],
     entry_points={
         "console_scripts": [
             "Rhapso = Rhapso.cli:main",
+            "featureMatchingHomeography_tiff = Rhapso.matching:featureMatchingHomeography_tiff",
         ],
     },
 )
