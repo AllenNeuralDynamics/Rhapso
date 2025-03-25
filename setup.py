228ec39d470e5e3e5258902f311dc2e53b607f79
Martin Barker <martinbarker99@gmail.com>
2025-03-25 10:37:44 -0700

    change version to v0.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Rhapso",
-    version="0.0.3",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         'dask[array]',  
