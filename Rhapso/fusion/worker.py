2d4e5cc09e2e20a97f04ac91e5ec0f760fb9bb07
seanfite-alleninstitute <sean.fite@alleninstitute.org>
2025-10-27 10:23:19 -0700

    Updated fusion and added scheduler inline

--- Rhapso/fusion/worker.py
+++ Rhapso/fusion/worker.py
@@ -1,92 +1,64 @@
 """
-NOTE:
-Codebase intended for GPU/CPU device.
-No fallback to CPU written until required.
+Runs fusion from config file generated
+from dispim or exaspim scheduler.
 """
 
-import uuid
-import time
-from pathlib import Path
-import xml.etree.ElementTree as ET
-import yaml
-import fusion.aind_cloud_fusion.fusion as fusion
-import fusion.aind_cloud_fusion.input_output as input_output
-import fusion.aind_cloud_fusion.script_utils as utils
-import xml.etree.ElementTree as ET
-import boto3
-from io import BytesIO
+from .affine_fusion import blend as blend
+from .affine_fusion import fusion as fusion
+from .affine_fusion import geometry as geometry
+from .affine_fusion import io as io
+from .affine_fusion import script_utils as script_utils
 
-def get_tile_zyx_resolution(input_xml_path: str) -> list[int]: 
+def execute_job(xml_path: str, image_data_input_path: str, output_s3_path: str, dataset_type: str, channel: int):
     """
-    Parse tile resolution to store in ome_ngff metadata
+    yml_path: Local yml path
+    xml_path: Local xml path
+    output_path: Local results path
     """
-    if input_xml_path.startswith('s3://'):
-        # Handle S3 path
-        s3 = boto3.resource('s3')
-        bucket_name, key = input_xml_path[5:].split('/', 1)
-        bucket = s3.Bucket(bucket_name)
-        obj = bucket.Object(key)
-        response = obj.get()
-        file_stream = BytesIO(response['Body'].read())
-        tree = ET.parse(file_stream)
-    else:
-        tree = ET.parse(input_xml_path)
-    
-    root = tree.getroot()
 
-    res_xyz = root.find('SequenceDescription').find('ViewSetups').find('ViewSetup').find('voxelSize').find('size').text
-    res_zyx = [float(num) for num in res_xyz.split(' ')[::-1]]
-    
-    return res_zyx
+    # Initialize Application Objects
+    # Application Object: DATASET
+    if dataset_type == 'BigStitcherDataset':
+        dataset = io.BigStitcherDataset(xml_path, image_data_input_path, datastore=0)
+    elif dataset_type == 'BigStitcherDatasetChannel':
+        dataset = io.BigStitcherDatasetChannel(xml_path, image_data_input_path, channel, datastore=0)
+    DATASET = dataset
 
-def execute_job(yml_path, xml_path, output_path):
-    # Prep inputs
-    configs = utils.read_config_yaml(yml_path)
-    input_path = configs['input_path']
-    output_s3_path = configs['output_path']
-    channel = configs['channel']
-
-    resolution_zyx = get_tile_zyx_resolution(xml_path)
-    output_params = input_output.OutputParameters(
+    # Application Object: OUTPUT_PARAMS
+    OUTPUT_PARAMS = io.OutputParameters(
         path=output_s3_path,
-        resolution_zyx=resolution_zyx
+        chunksize=(1, 1, 640, 256, 256),    # NOTE: Please select your output chunk size
+        resolution_zyx=DATASET.tile_resolution_zyx,   # NOTE: Please select your output resolution
+        datastore=0
     )
-    blend_option = 'weighted_linear_blending'
+
+    # Application Parameter: CELL_SIZE
+    CELL_SIZE = [640, 256, 256]     # NOTE: Please set this to = output chunk size
+
+    # Application Parameter: POST_REG_TFMS
+    POST_REG_TFMS: list[geometry.Affine] = []   # NOTE: Please add optional post-reg transforms
+
+    _, _, _, _, tile_aabbs, _, _ = fusion.initialize_fusion(DATASET, POST_REG_TFMS, OUTPUT_PARAMS)
+
+    # Application Object: BLENDING_MODULE
+    BLENDING_MODULE = blend.WeightedLinearBlending(tile_aabbs)     # NOTE: Please choose your desired blending
+    # BLENDING_MODULE = blend.MaxProjection() 
 
     # Run fusion
     fusion.run_fusion(
-            input_path,
-            xml_path,
-            channel,
-            output_params,
-            blend_option
+            DATASET,
+            OUTPUT_PARAMS,
+            CELL_SIZE,
+            POST_REG_TFMS,
+            BLENDING_MODULE,
     )
-
-    # Log 'done' file for next capsule in pipeline.
-    # Unique log filename
-    unique_id = str(uuid.uuid4())
-    timestamp = int(time.time() * 1000)
-    unique_file_name = str(Path(output_path) / f"file_{timestamp}_{unique_id}.yml")
-
-    log_content = {}
-    log_content['in_path'] = output_params.path
-    log_content['output_path'] = output_params.path.replace("fused_full_res", "fused")
-    log_content['resolution_zyx'] = list(output_params.resolution_zyx)
-
-    with open(unique_file_name, "w") as file:
-        yaml.dump(log_content, file)
-
 
 if __name__ == '__main__':
 
-    xml_path = 's3://rhapso-zarr-glue/output/dataset-solve.xml'
-    yml_path = 's3://rhapso-zarr-glue/output/worker_config.yml'
-    output_path = 's3://rhapso-zarr-glue/output/'
+    xml_path = "s3://aind-open-data/HCR_802704_2025-08-30_02-00-00_processed_2025-10-01_21-09-24/image_tile_alignment/bigstitcher.xml"
+    image_data_input_path = "s3://aind-open-data/HCR_802704_2025-08-30_02-00-00_processed_2025-10-01_21-09-24/image_radial_correction/"
+    output_s3_path = "s3://sean-fusion/output5/channel_488.zarr"
+    dataset_type = "BigStitcherDataset"
+    channel = None                      # list channel num (int) if fusing a specific channel from an xml of multiple channels
 
-    print(f'{xml_path=}')
-    print(f'{yml_path=}')
-    print(f'{output_path=}')
-
-    execute_job(yml_path,
-                xml_path,
-                output_path)
+    execute_job(xml_path, image_data_input_path, output_s3_path, dataset_type, channel)
